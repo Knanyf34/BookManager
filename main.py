b6bd3951e85c9da28753a3dbd47ea55ffc553e06
shamimdanesh <me@shamimdanesh.com>
2022-08-31 19:09:44 +0430

    comment all outputs

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ if __name__ == '__main__':
     b2 = Book('Java Spring', 1000)
     b3 = Book('Java Spring', 1000)
 
-    print(b1)
+    # print(b1)
     # print('b1 title: '+b1.get_title())
     # print('b1 pages: ' + str(b1.get_pages()))
-    print(b2)
-    print(b3)
-
-    if b2 == b3:
-        print('Gholam')
-    else:
-        print('Ghamar')
+    # print(b2)
+    # print(b3)
+    #
+    # if b2 == b3:
+    #     print('Gholam')
+    # else:
+    #     print('Ghamar')
 
 
     # print(b1 + b2)
@@ -32,16 +32,18 @@ if __name__ == '__main__':
     # Search List!
 
     # Exception
-    try:
-        b1.set_pages(-100)
-        b1.set_pages('Jafar')
-        # b1.set_pages(55)
-    except Exception as jafar:
-        print(jafar)
-    else:
-        print("Hooooraaaa!")
-    finally:
-        print("Vel kon BABA...")
+    # try:
+    #     b1.set_pages(-100)
+    #     b1.set_pages('Jafar')
+    #     # b1.set_pages(55)
+    # except Exception as jafar:
+    #     print(jafar)
+    # else:
+    #     print("Hooooraaaa!")
+    # finally:
+    #     print("Vel kon BABA...")
+    #
+    # b1.set_pages(50)
+    # print(b1)
 
-    b1.set_pages(50)
-    print(b1)
+    print('Ana Development!!')
